b8a2d86cd7b91225bdcd4714678e5eafed473873
Teicheto <164079602+Teicheto@users.noreply.github.com>
2025-03-28 15:29:01 +0200

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -1,11 +1,11 @@
 async function fetchPlayers() {
     try {
-        let response = await fetch(`https://api.mcsrvstat.us/2/play.icemc.online`);
+        let response = await fetch("https://api.mcsrvstat.us/2/play.icemc.online");
         let data = await response.json();
-        
-        console.log("Minecraft Server Data:", data); // Debugging - виж в конзолата на браузъра
 
-        if (data.online) {
+        console.log("Minecraft Server Data:", data); // Debugging
+
+        if (data.online && data.players) {
             document.getElementById("players-online").innerText = data.players.online;
         } else {
             document.getElementById("players-online").innerText = "Сървърът е офлайн";
@@ -16,47 +16,47 @@ async function fetchPlayers() {
     }
 }
 
-// Извикване на функцията при зареждане на страницата
+// Стартиране при зареждане на страницата
 fetchPlayers();
 setInterval(fetchPlayers, 30000); // Обновяване на всеки 30 секунди
 
-function createStar() {
-    let star = document.createElement("div");
-    star.classList.add("falling-star");
-    star.style.left = Math.random() * 100 + "vw";
-    star.style.animationDuration = Math.random() * 3 + 2 + "s";
-    document.body.appendChild(star);
 
-    setTimeout(() => { star.remove(); }, 5000);
+function createStar() {
+let star = document.createElement("div");
+star.classList.add("falling-star");
+star.style.left = Math.random() * 100 + "vw";
+star.style.animationDuration = Math.random() * 3 + 2 + "s";
+document.body.appendChild(star);
+
+setTimeout(() => { star.remove(); }, 5000);
 }
 
 setInterval(createStar, 300);
 
 function copyIP() {
-    const ip = "play.icemc.online"; // IP на сървъра
-    navigator.clipboard.writeText(ip).then(() => {
-        alert("IP адресът е копиран: " + ip);
-    }).catch(err => {
-        console.error("Грешка при копиране: ", err);
-    });
+const ip = "play.icemc.online"; // Твоето IP
+navigator.clipboard.writeText(ip).then(() => {
+alert("IP адресът е копиран: " + ip);
+}).catch(err => {
+console.error("Грешка при копиране: ", err);
+});
 }
 
+
+
 async function fetchDiscordMembers() {
-    const guildId = "1320671930239684690"; // Трябва да замениш с твоето Discord Guild ID
-    const widgetUrl = `https://discord.com/api/guilds/${guildId}/widget.json`;
+const guildId = "1320671930239684690"; // Трябва да замениш с твоето Discord Guild ID
+const widgetUrl = `https://discord.com/api/guilds/${guildId}/widget.json`;
 
-    try {
-        const response = await fetch(widgetUrl);
-        if (!response.ok) throw new Error("Грешка при взимане на данни!");
+try {
+const response = await fetch(widgetUrl);
+if (!response.ok) throw new Error("Грешка при взимане на данни!");
 
-        const data = await response.json();
-        console.log("Discord Server Data:", data); // Debugging
-
-        document.getElementById("discord-online").innerText = data.presence_count;
-    } catch (error) {
-        console.error("Грешка при зареждане на Discord данни:", error);
-        document.getElementById("discord-online").innerText = "Няма информация";
-    }
+const data = await response.json();
+document.getElementById("discord-online").innerText = data.presence_count;
+} catch (error) {
+document.getElementById("discord-online").innerText = "Няма информация";
+}
 }
 
 // Извиква се при зареждане на страницата

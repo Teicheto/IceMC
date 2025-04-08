b08bb6825764ed654ee69053169984e2349244f7
RandomTechrate <nikatajaged@gmail.com>
2025-04-08 21:25:00 +0300

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -1,6 +1,6 @@
 async function fetchPlayers() {
     try {
-        let response = await fetch(`https://mcsrvstat.us/server/play.icemc.online`);
+        let response = await fetch(`https://api.mcsrvstat.us/3/play.icemc.online`);
         let data = await response.json();
         if (data.online) {
             document.getElementById("players-online").innerText = data.players.now;
